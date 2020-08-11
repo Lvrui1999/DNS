9227cecd91c493400cc89a5c0bce7883ce63ab68
Lvrui1999 <lvrui1999@gmail.com>
2020-08-11 11:38:11 +0800

    asdad

---
+++ cache.h
@@ -0,0 +1,34 @@
+#define CacheSize 1024
+
+typedef struct Cache{
+    char* name[CacheSize];
+    int ip[CacheSize];
+    int last[CacheSize];
+    int size;
+};
+
+int lru(Cache c){
+    int i;
+    int pos = 0;
+    int pval = -1;
+    for(i = 0; i < c.size; i++){
+        if(c.last[i] > pval){
+            pval = c.last[i];
+            pos = i;
+        }
+    }
+    return pos;
+}
+
+void add(char* name, int ip, Cache c){
+    if(c.size < CacheSize){
+        c.size++;
+        c.name[c.size] = name;
+        c.ip[c.size] = ip;
+    }
+    else{
+        int pos = lru(c);
+        c.name[pos] = name;
+        c.ip[pos] = ip;
+    }
+}
